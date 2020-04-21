5ef87471834d7a32edaa690631eeb74b53dd308c
OMER BIN ALI <omerbinali311@gmail.com>
2020-04-21 20:07:43 +0530

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,17 @@
+
+from twilio.rest import Client
+
+
+
+account_sid = 'ACeff2f700c5390385cd83efbae31d9a29'
+auth_token = 'f43da301b051e58787fd5d29b8cdc1a7'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="kikikikikikikiki   haahahhaa lololo",
+                     from_='+18596578314',
+                     to='+917674982987'
+                 )
+
+print(message.sid)
